439501227029c0b105ed754e9c4c172ad2df2a49
prakash <prakashreddy9710@gmail.com>
2025-10-14 15:22:28 -0400

    added the deplyment docs

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This 'base' property is essential for GitHub Pages deployment
+  base: '/MyPortfolio/',
+});
